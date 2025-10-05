5a6460db5838634711d53fbe62dcbc50c26ecec2
Scott Bakker <sbakker343@gmail.com>
2025-10-05 16:21:52 -0400

    parser with directions, prepositions, indirect nouns

--- Grif/Grif.Parser.cs
+++ Grif/Grif.Parser.cs
@@ -3,13 +3,31 @@ using static Grif.Common;
 
 namespace Grif;
 
+/*
+Can handle the following input patterns:
+    verb
+    direction
+    verb direction
+    verb noun
+    verb noun preposition indirectnoun
+    verb preposition noun
+The order of the words does not matter, so "go west" and "west go" are equivalent.
+Noun must come before indirect noun if both are present.
+*/
+
 public static partial class Grif
 {
+    private static int _maxWordLen = 0;
     private const string VERB_PREFIX = "verb.";
     private const string NOUN_PREFIX = "noun.";
+    private const string DIRECTION_PREFIX = "direction.";
+    private const string PREPOSITION_PREFIX = "preposition.";
+    private const string COMMAND_PREFIX = "command.";
+    private static string DONT_UNDERSTAND_TEXT = "";
     private static List<GrodItem> _verbs = [];
     private static List<GrodItem> _nouns = [];
-    private static int _maxWordLen = 0;
+    private static List<GrodItem> _directions = [];
+    private static List<GrodItem> _prepositions = [];
 
     [SuppressMessage("Style", "IDE0305:Simplify collection initialization")]
     public static void ParseInit(Grod grod)
@@ -22,12 +40,24 @@ public static partial class Grif
             .Where(x => !string.IsNullOrWhiteSpace(x.Value) && x.Value != NULL)
             .Select(x => new GrodItem(x.Key[NOUN_PREFIX.Length..], x.Value))
             .ToList();
+        _directions = grod.Items(DIRECTION_PREFIX, true, true)
+            .Where(x => !string.IsNullOrWhiteSpace(x.Value) && x.Value != NULL)
+            .Select(x => new GrodItem(x.Key[DIRECTION_PREFIX.Length..], x.Value))
+            .ToList();
+        _prepositions = grod.Items(PREPOSITION_PREFIX, true, true)
+            .Where(x => !string.IsNullOrWhiteSpace(x.Value) && x.Value != NULL)
+            .Select(x => new GrodItem(x.Key[PREPOSITION_PREFIX.Length..], x.Value))
+            .ToList();
         _maxWordLen = Dags.GetIntValue(grod.Get(WORDSIZE, true));
         if (_maxWordLen > 0)
         {
             TrimSynonyms(ref _verbs);
             TrimSynonyms(ref _nouns);
+            TrimSynonyms(ref _directions);
+            TrimSynonyms(ref _prepositions);
         }
+        DONT_UNDERSTAND_TEXT = grod.Get(DONT_UNDERSTAND, true) ??
+            "I don't understand \"{0}\".\\n";
     }
 
     public static List<DagsItem>? ParseInput(Grod grod, string input)
@@ -35,8 +65,14 @@ public static partial class Grif
         var result = new List<DagsItem>();
         string? verb;
         string? verbWord;
+        string? direction = null;
+        string? directionWord = null;
         string? noun = null;
         string? nounWord = null;
+        string? preposition = null;
+        string? prepositionWord = null;
+        string? indirectNoun = null;
+        string? indirectNounWord = null;
         if (string.IsNullOrWhiteSpace(input))
         {
             return null;
@@ -47,46 +83,115 @@ public static partial class Grif
         {
             return null;
         }
-        (verb, verbWord) = GetVerb(ref words);
-        if (verb == null)
+        // handle "west", "go west", "west go"
+        (verb, verbWord) = GetMatchingWord(_verbs, ref words);
+        if (words.Count > 0)
         {
-            result.Add(new DagsItem(DagsType.Text, $"I don't understand \"{input}\".\\n"));
+            (direction, directionWord) = GetMatchingWord(_directions, ref words);
+        }
+        if (words.Count > 0)
+        {
+            (noun, nounWord) = GetNoun(_nouns, ref words);
+        }
+        if (words.Count > 0)
+        {
+            (preposition, prepositionWord) = GetMatchingWord(_prepositions, ref words);
+            if (words.Count > 0)
+            {
+                (indirectNoun, indirectNounWord) = GetNoun(_nouns, ref words);
+            }
+            else
+            {
+                result.Add(new DagsItem(DagsType.Text, string.Format(DONT_UNDERSTAND_TEXT, input)));
+                return result;
+            }
+        }
+        if (verb == null && direction == null)
+        {
+            result.Add(new DagsItem(DagsType.Text, string.Format(DONT_UNDERSTAND_TEXT, input)));
             return result;
         }
         if (words.Count > 0)
         {
-            (noun, nounWord) = GetNoun(ref words);
-            if (noun == null)
-            {
-                result.Add(new DagsItem(DagsType.Text, $"I don't understand \"{input}\".\\n"));
-                return result;
-            }
+            result.Add(new DagsItem(DagsType.Text, string.Format(DONT_UNDERSTAND_TEXT, string.Join(", ", words))));
+            return result;
         }
-        var command = $"command.{verb}";
+        string command;
+        if (verb != null && direction != null)
+        {
+            command = $"{COMMAND_PREFIX}{verb}.{direction}";
+        }
+        else if (direction != null)
+        {
+            command = $"{COMMAND_PREFIX}{direction}";
+            verb = direction;
+            verbWord = directionWord;
+            direction = null;
+            directionWord = null;
+        }
+        else
+        {
+            command = $"{COMMAND_PREFIX}{verb}";
+        }
         if (noun != null)
         {
-            command += $".{noun}";
-            if (grod.Get(command, true) == null && int.TryParse(nounWord, out int nounNumber))
+            if (preposition != null && indirectNoun != null)
             {
-                command = $"command.{verb}.#"; // numeric noun
-                noun = "#";
-                nounWord = nounNumber.ToString();
+                var tempCommand = $"{command}.{noun}.{preposition}.{indirectNoun}";
+                if (grod.Get(tempCommand, true) != null)
+                {
+                    command = tempCommand;
+                }
+                else
+                {
+                    tempCommand = $"{command}.{noun}.{preposition}.*"; // any indirect noun
+                    if (grod.Get(tempCommand, true) != null)
+                    {
+                        command = tempCommand;
+                    }
+                    else
+                    {
+                        command = $"{command}.*.{preposition}.*"; // any noun, any indirect noun
+                    }
+                }
             }
-            if (grod.Get(command, true) == null)
+            else if (preposition != null) // no indirect noun
             {
-                command = $"command.{verb}.*"; // any noun
+                var tempCommand = $"{command}.{preposition}.{noun}";
+                if (grod.Get(tempCommand, true) != null)
+                {
+                    command = tempCommand;
+                }
+                else
+                {
+                    command = $"{command}.{preposition}.*"; // any noun
+                }
+            }
+            else if (grod.Get($"{command}.{noun}", true) != null)
+            {
+                command += $".{noun}";
+            }
+            else
+            {
+                command += ".*"; // any noun
             }
         }
         if (grod.Get(command, true) == null)
         {
-            result.Add(new DagsItem(DagsType.Text, $"I don't understand \"{input}\".\\n"));
+            result.Add(new DagsItem(DagsType.Text, string.Format(DONT_UNDERSTAND_TEXT, input)));
             return result;
         }
         result.Add(new DagsItem(DagsType.Internal, $"@set(input.full,\"{input}\")"));
-        result.Add(new DagsItem(DagsType.Internal, $"@set(input.verb,{verb})"));
-        result.Add(new DagsItem(DagsType.Internal, $"@set(input.verbword,{verbWord})"));
-        result.Add(new DagsItem(DagsType.Internal, $"@set(input.noun,{noun ?? "\"\""})"));
-        result.Add(new DagsItem(DagsType.Internal, $"@set(input.nounword,{nounWord ?? "\"\""})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.verb,{verb ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.verbword,{verbWord ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.direction,{direction ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.directionword,{directionWord ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.noun,{noun ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.nounword,{nounWord ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.preposition,{preposition ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.prepositionword,{prepositionWord ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.indirectnoun,{indirectNoun ?? NULL})"));
+        result.Add(new DagsItem(DagsType.Internal, $"@set(input.indirectnounword,{indirectNounWord ?? NULL})"));
         result.Add(new DagsItem(DagsType.Internal, $"@script({command})"));
         return result;
     }
@@ -107,7 +212,7 @@ public static partial class Grif
         }
     }
 
-    private static (string?, string?) GetVerb(ref List<string> words)
+    private static (string?, string?) GetMatchingWord(List<GrodItem> vocabList, ref List<string> words)
     {
         for (int i = 0; i < words.Count; i++)
         {
@@ -116,7 +221,7 @@ public static partial class Grif
             {
                 word = word[.._maxWordLen];
             }
-            foreach (var item in _verbs)
+            foreach (var item in vocabList)
             {
                 var verbWords = item.Value!.Split(',', StringSplitOptions.RemoveEmptyEntries);
                 foreach (var syn in verbWords)
@@ -133,36 +238,19 @@ public static partial class Grif
         return (null, null);
     }
 
-    private static (string?, string?) GetNoun(ref List<string> words)
+    private static (string?, string?) GetNoun(List<GrodItem> nounList, ref List<string> words)
     {
-        for (int i = 0; i < words.Count; i++)
+        (var noun, var nounWord) = GetMatchingWord(nounList, ref words);
+        if (noun != null)
         {
-            var word = words[i];
-            if (_maxWordLen > 0 && word.Length > _maxWordLen)
-            {
-                word = word[.._maxWordLen];
-            }
-            foreach (var item in _nouns)
-            {
-                var nounWords = item.Value!.Split(',', StringSplitOptions.RemoveEmptyEntries);
-                foreach (var syn in nounWords)
-                {
-                    if (word.Equals(syn, OIC))
-                    {
-                        word = words[i]; // use original string
-                        words.RemoveAt(i);
-                        return (item.Key, word);
-                    }
-                }
-            }
+            return (noun, nounWord);
         }
         for (int i = 0; i < words.Count; i++)
         {
             if (int.TryParse(words[i], out int number))
             {
-                var word = words[i];
                 words.RemoveAt(i);
-                return ("#", word); // numeric noun
+                return ("#", number.ToString()); // numeric noun, normalized
             }
         }
         return (null, null);
